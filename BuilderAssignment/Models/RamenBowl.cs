744d7fc8f845ae83e90aee2da565ce59b2766113
Your Name <shivaadhikari0214@gmail.com>
2023-10-11 00:43:00 +0300

    Added the attributes and description of finishedbowlOfRamen.

--- BuilderAssignment/Models/RamenBowl.cs
+++ BuilderAssignment/Models/RamenBowl.cs
@@ -6,8 +6,6 @@ using System.Threading.Tasks;
 
 namespace BuilderAssignment.Models
 {
-    internal class RamenBowl
-    {
         /// <summary>
         /// This class represents the final bowl of ramen
         /// It should have attributes like broth, type of noodle, toppings and extras
@@ -15,12 +13,19 @@ namespace BuilderAssignment.Models
         public class RamenBowl
         {
             //TODO#1: Add attributes for each enum in IngredientEnums.cs file
+            public string? BrothType{get; set;}
+            
+            public string? NoodleType { get; set; }
 
+            public string? Toppings { get; set; }
+
+            public string? Extras { get; set; }
             //TODO#2: Return description of finished bowl of ramen
-            public static string FinishedBowlOfRamen()
+            public static string FinishedBowlOfRamen(RamenBowl ramen)
             {
-                return "";
+                 return  "This bowl of ramen contains: \n" + $"Broth:{ramen.BrothType}" + $"Noodles:{ramen.NoodleType}" + $"Toppings:{ramen.Toppings}" + $"Extras:{ramen.Extras}";
+               
             }
         }
     }
-}
+
