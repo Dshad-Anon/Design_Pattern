b7bcfe40bc8e98b589c1ed4ff3301d99acde2c48
Your Name <shivaadhikari0214@gmail.com>
2023-10-15 01:19:36 +0300

    Implementation of main to make ramen. Also change to extras null implementation.

--- BuilderAssignment/Models/RamenBowl.cs
+++ BuilderAssignment/Models/RamenBowl.cs
@@ -23,9 +23,30 @@ namespace BuilderAssignment.Models
             //TODO#2: Return description of finished bowl of ramen
             public static string FinishedBowlOfRamen(RamenBowl ramen)
             {
-                 return  "This bowl of ramen contains: \n" + $"Broth:{ramen.BrothType}\n" + $"Noodles:{ramen.NoodleType}\n" + $"Toppings:{ramen.Toppings}\n" + $"Extras:{ramen.Extras}\n";
-               
+            // If string is null in extra.The whole extra attritube is showing so just to implement it I make another return statement.
+            // return  "This bowl of ramen contains: \n" + $"Broth:{ramen.BrothType}\n" 
+            //+ $"Noodles:{ramen.NoodleType}\n"
+            //+ $"Toppings:{ramen.Toppings}\n" + $"Extras:{ramen.Extras}\n";
+            string description = "This bowl of ramen contains:";
+            if (!string.IsNullOrEmpty(ramen.BrothType))
+            {
+                description += $" Broth: {ramen.BrothType} \n";
             }
+            if (!string.IsNullOrEmpty(ramen.NoodleType))
+            {
+                description += $" Noodles: {ramen.NoodleType}\n";
+            }
+            if (!string.IsNullOrEmpty(ramen.Toppings))
+            {
+                description += $" Topping: {ramen.Toppings}\n";
+            }
+            if (!string.IsNullOrEmpty(ramen.Extras))
+            {
+                description += $" Extra: {ramen.Extras}\n";
+            }
+            return description;
+
+        }
         }
     }
 
--- BuilderAssignment/Program.cs
+++ BuilderAssignment/Program.cs
@@ -10,19 +10,23 @@ namespace BuilderAssignment
         {
             // TODO#1: Wake up your RamenChef by initializing it
             RamenChef chef = new RamenChef();
+            // TODO#2: Create new RamenBuilder's for each ramen recepies (MisoRamenBuilder, SpicyRamenBuilder, VeganRamenBuilder)
+            // TODO#3: Use your RamenChef to prepare (build) each type of ramen (Miso, Spicy, Vegan)
             RamenBowl misoRamen = chef.PrepareRamen(new MisoRamenBuilder());
+            RamenBowl veganRamen = chef.PrepareRamen(new VeganRamenBuilder());
+            RamenBowl spicyRamen = chef.PrepareRamen(new MisoRamenBuilder());
+
+
+            // TODO#4: After the RamenCheft has prepared the bowl of ramen, use FinishedBowlOfRamen to get the content of
+            //         the RamenBowl and print it out using WriteLine method
             RamenBowl.FinishedBowlOfRamen(misoRamen);
             Console.WriteLine(RamenBowl.FinishedBowlOfRamen(misoRamen));
 
-            RamenBowl veganRamen = chef.PrepareRamen(new VeganRamenBuilder());
+            RamenBowl.FinishedBowlOfRamen(veganRamen);
+            Console.WriteLine(RamenBowl.FinishedBowlOfRamen(veganRamen));
 
-
-
-            // TODO#2: Create new RamenBuilder's for each ramen recepies (MisoRamenBuilder, SpicyRamenBuilder, VeganRamenBuilder) 
-            // TODO#3: Use your RamenChef to prepare (build) each type of ramen (Miso, Spicy, Vegan)
-            // TODO#4: After the RamenCheft has prepared the bowl of ramen, use FinishedBowlOfRamen to get the content of
-            //         the RamenBowl and print it out using WriteLine method
-
+            RamenBowl.FinishedBowlOfRamen(spicyRamen);
+            Console.WriteLine(RamenBowl.FinishedBowlOfRamen(spicyRamen));
         }
     }
 }
