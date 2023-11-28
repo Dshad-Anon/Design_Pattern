b87e14330d22063e2b68df847bb1151f7180ceb7
Your Name <shivaadhikari0214@gmail.com>
2023-11-28 03:54:42 +0200

    Implementation of all menus separetely and also the waitress class.

--- IteratorAndCompositeExercise/Program.cs
+++ IteratorAndCompositeExercise/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using IteratorAndCompositeExercise.Menus;
+using System.Collections;
 using System.Drawing;
 
 namespace IteratorAndCompositeExercise
@@ -10,129 +11,13 @@ namespace IteratorAndCompositeExercise
     {
         static void Main(string[] args)
         {
+            //
+            PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+            DinerMenu dinerMenu = new DinerMenu();
+
+            Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+            waitress.PrintMenu();
             
-        }
-
-        public class WaitressSpec
-        {
-            public void printMenu()
-            {
-                PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
-                ArrayList breakfastitems = pancakeHouseMenu.GetMenuItems();
-
-                DinerMenu dinerMenu = new DinerMenu();
-                MenuItem[] lunchItems = dinerMenu.GetMenuItems();
-
-                for (int i = 0; i < breakfastitems.Count; i++)
-                {
-                    MenuItem? menuItem = (MenuItem?)breakfastitems[i];
-                    Console.WriteLine(menuItem.GetName + " ");
-                    Console.WriteLine(menuItem.GetPrice() + " ");
-                    Console.WriteLine(menuItem.GetDescription() + " ");
-                }
-                for(int i = 0; i < lunchItems.Length; i++)
-                {
-                    MenuItem menuItem = lunchItems[i];
-                    Console.WriteLine(menuItem.GetName() + " ");
-                    Console.WriteLine(menuItem.GetPrice() + " ");
-                    Console.WriteLine(menuItem.GetDescription() + " ");
-
-                }
-            }
-        }
-        public class MenuItem
-        {
-            public string Name { get; set; }
-            public string Description { get; set; }
-            public bool Vegetarian { get; set; }
-            public double Price { get; set; }
-
-            public MenuItem(string name, string description, bool vegetarian, double price)
-            {
-                Name = name;
-                Description = description;
-                Vegetarian = vegetarian;
-                Price = price;
-            }
-
-            public string GetName()
-            {
-                return Name;
-            }
-
-            public string GetDescription()
-            {
-                return Description;
-            }
-
-            public Boolean GetVegetarian()
-            {
-                return Vegetarian;
-            }
-
-            public double GetPrice()
-            {
-                return Price;
-            }
-        }
-
-        public class PancakeHouseMenu
-        {
-            public ArrayList MenuItems { get; set; }
-
-            public PancakeHouseMenu()
-            {
-                MenuItems = new ArrayList();
-
-                AddItem("K&B´s Pancake Breakfast", "Pancakes with scrambled eggs, and toast", false, 2.99);
-            }
-
-            public void AddItem(string name, string description, bool vegetarian, double price)
-            {
-                MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
-                MenuItems.Add(menuItem);
-            }
-
-            public ArrayList GetMenuItems()
-            {
-                return MenuItems;
-            }
-        }
-
-        public class DinerMenu
-        {
-            private int Max_Items = 6;
-            int numberOfItems = 0;
-            MenuItem[] MenuItems;
-
-            public DinerMenu()
-            {
-                MenuItems = new MenuItem[Max_Items];
-
-                AddItem("Vegetarian BLT", "(Fakin´) Bacon with lettuce & tomato on whole wheat", true, 2.99);
-                AddItem("BLT","Bacon with lettuce & tomato on whole wheat\n", false, 2.99);
-                AddItem("Soup of the day", "Soup of the day, with a side of potato salad", false, 3.29);
-                AddItem("HotDog", "A hot dog, with saurkraut, relish, onions, topped with cheese",false,3.29);
-            }
-
-            public void AddItem(string name, string description, bool vegetarian, double price)
-            {
-                MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
-                if (numberOfItems >= Max_Items)
-                {
-                    Console.WriteLine("Sorry, menu is full! Can't add item to menu");
-                }
-                else
-                {
-                    MenuItems[numberOfItems] = menuItem;
-                    numberOfItems++;
-                }
-            }
-
-            public MenuItem[] GetMenuItems()
-            {
-                return MenuItems;
-            }
         }
     }
 }
